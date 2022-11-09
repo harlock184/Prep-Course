e972bcc4ccb2c7bc2bc58207d781ca8a0a66b4e5
harlock184 <105575194+harlock184@users.noreply.github.com>
2022-11-09 17:02:50 -0600

    Tarea 1 terminada

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -276,21 +276,15 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  //var contador = 0
-  var vocales = ["A","E","I","O","U","a","e","i","o","u"];
-  for (var i = 0; i<letra.length; i++){
-    if(letra.length = vocales){
-      return "Es vocal"
-      
-    }else if (letra.length > 1){
 
-      return "Dato incorrecto"
-    }
+  if(letra.length>1){
+    return "Dato incorrecto"
 
-  
+  }if(letra==="a" || letra ==="e" || letra === "i" || letra === "o" || letra==="u"){
+    return "Es vocal"
   }
+  return "Dato incorrecto"
 
-  
 }
 
 
